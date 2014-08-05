bbb371426fe69ca2bb734b8598ea0a603323d1d0
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2014-08-05 07:42:55 +0200

    [BUILD] made examples build more generic
    and luawrapper-lua_dep target is now functional

--- atom.mk
+++ atom.mk
@@ -4,9 +4,6 @@ LOCAL_PATH := $(call my-dir)
 # luawrapper-no_dep
 ###############################################################################
 
-# TODO doesn't work as is, because make final strips (and breaks) the executable
-# TODO breaks when parallelized
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := luawrapper-no_dep
@@ -18,20 +15,23 @@ LOCAL_SRC_FILES := luawrapper.c \
 	test/no_dep/lw_dependencies.c \
 	$(call all-c-files-under,lua)
 
-luawrapper_no_dep_BUILD_DIR := $(call local-get-build-dir)
-luawrapper_no_dep := $(luawrapper_no_dep_BUILD_DIR)/$(LOCAL_MODULE)
-luawrapper_no_dep_dependencies := \
-	$(luawrapper_no_dep) \
+$(LOCAL_MODULE)_BUILD_DIR := $(call local-get-build-dir)
+$(LOCAL_MODULE) := $($(LOCAL_MODULE)_BUILD_DIR)/$(LOCAL_MODULE)
+$(LOCAL_MODULE)_dependencies := \
+	$($(LOCAL_MODULE)) \
 	$(LOCAL_PATH)/test/no_dep/main.lua
 
 LOCAL_LDFLAGS := -static -lm
 
+# TODO replace by an embedded libelf
+LOCAL_LIBRARIES := libelf
+
 define LOCAL_CMD_PRE_INSTALL
-	echo "generate full wrapper for $(PRIVATE_MODULE)"
+	@echo "generate full wrapper for $(PRIVATE_MODULE)"
 	$(Q) TARGET_OBJCOPY=$(TARGET_OBJCOPY) \
-		$(PRIVATE_PATH)/build_wrapper.sh -o $(luawrapper_no_dep).tmp \
-		$(luawrapper_no_dep_dependencies)
-	$(Q) mv $(luawrapper_no_dep).tmp $(luawrapper_no_dep)
+		$(PRIVATE_PATH)/build_wrapper.sh -o $($(PRIVATE_MODULE)).tmp \
+		$($(PRIVATE_MODULE)_dependencies)
+	$(Q) mv $($(PRIVATE_MODULE)).tmp $($(PRIVATE_MODULE))
 endef
 
 include $(BUILD_EXECUTABLE)
@@ -39,8 +39,6 @@ include $(BUILD_EXECUTABLE)
 ###############################################################################
 # luawrapper-lua_dep
 ###############################################################################
-
-# TODO doesn't work as is, because make final strips (and breaks) the executable
 
 include $(CLEAR_VARS)
 
@@ -53,42 +51,24 @@ LOCAL_SRC_FILES := luawrapper.c \
 	test/lua_dep/lw_dependencies.c \
 	$(call all-c-files-under,lua)
 
-luawrapper_lua_dep_BUILD_DIR := $(call local-get-build-dir)
-luawrapper_lua_dep := $(luawrapper_lua_dep_BUILD_DIR)/$(LOCAL_MODULE)
-luawrapper_lua_dep_staging := $(TARGET_OUT_STAGING)/usr/bin/$(LOCAL_MODULE)
-luawrapper_lua_dep_done := $(luawrapper_lua_dep_BUILD_DIR)/$(LOCAL_MODULE).done
-luawrapper_lua_dep_absolute_scr_files := \
-	$(foreach s,$(LOCAL_SRC_FILES),$(LOCAL_PATH)/$(s))
-luawrapper_lua_dep_dependencies := \
-	$(luawrapper_lua_dep)-empty \
+$(LOCAL_MODULE)_BUILD_DIR := $(call local-get-build-dir)
+$(LOCAL_MODULE) := $($(LOCAL_MODULE)_BUILD_DIR)/$(LOCAL_MODULE)
+$(LOCAL_MODULE)_dependencies := \
+	$($(LOCAL_MODULE)) \
 	$(LOCAL_PATH)/test/lua_dep/foo.lua \
 	$(LOCAL_PATH)/test/lua_dep/main.lua
 
-$(luawrapper_lua_dep_done): $(luawrapper_lua_dep_staging)
-	$(Q) touch $@
+LOCAL_LDFLAGS := -static -lm
 
-$(luawrapper_lua_dep_staging): $(luawrapper_lua_dep)
-	$(Q) cp -f $< $@
+# TODO replace by an embedded libelf
+LOCAL_LIBRARIES := libelf
 
-$(luawrapper_lua_dep)-empty: $(luawrapper_lua_dep_absolute_scr_files)
-	@echo "compile empty wrapper for $(PRIVATE_MODULE)"
-	$(Q) mkdir -p $(luawrapper_lua_dep_BUILD_DIR)
-	$(Q) $(CCACHE) $(TARGET_CC) \
-		-I$(PRIVATE_PATH) -I$(PRIVATE_PATH)/lua -Os \
-		$^ -o $@ \
-		-static -lm -Wl,--gc-sections
-	$(Q) $(TARGET_STRIP) $@
-
-$(luawrapper_lua_dep):$(luawrapper_lua_dep_dependencies)
+define LOCAL_CMD_PRE_INSTALL
 	@echo "generate full wrapper for $(PRIVATE_MODULE)"
-	$(Q) $(PRIVATE_PATH)/build_wrapper.sh -o $@ $^
+	$(Q) TARGET_OBJCOPY=$(TARGET_OBJCOPY) \
+		$(PRIVATE_PATH)/build_wrapper.sh -o $($(PRIVATE_MODULE)).tmp \
+		$($(PRIVATE_MODULE)_dependencies)
+	$(Q) mv $($(PRIVATE_MODULE)).tmp $($(PRIVATE_MODULE))
+endef
 
-.PHONY: $(PACKAGE_NAME)-clean
-# TODO does remove only luawrapper_lua_dep_done, why ?
-$(PACKAGE_NAME)-clean:
-	$(Q) rm -Rf $(luawrapper_lua_dep_staging)
-	$(Q) rm -Rf $(luawrapper_lua_dep_done)
-	$(Q) rm -Rf $(luawrapper_lua_dep)-empty
-	$(Q) rm -Rf $(luawrapper_lua_dep)
-
-include $(BUILD_CUSTOM)
+include $(BUILD_EXECUTABLE)
