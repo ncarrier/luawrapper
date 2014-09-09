2d01e0278b68a88c3bf3da86a935c621df7259fc
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2014-09-09 07:12:10 +0200

    [BUILD] extract test specific data from main tests makefile

---
+++ test/tests.mk
@@ -0,0 +1,14 @@
+# list of the tests to build
+tests := both_dep \
+	lua_dep \
+	no_dep
+
+# dependencies for each test
+both_dep_OBJS := both_dep/bar.o
+both_dep_LUA_DEPS := both_dep/foo.lua \
+	both_dep/main.lua
+
+lua_dep_LUA_DEPS := plop:lua_dep/foo.lua \
+	lua_dep/main.lua
+
+no_dep_LUA_DEPS := no_dep/main.lua
