2649a49a22894d060ca066c911bde913a843bb7a
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2014-09-12 07:09:28 +0200

    [BUILD] added a check target to examples

--- examples/examples.mk
+++ examples/examples.mk
@@ -7,8 +7,11 @@ examples := both_dep \
 both_dep_OBJS := both_dep/bar.o
 both_dep_LUA_DEPS := both_dep/foo.lua \
 	both_dep/main.lua
+both_dep_CHECK_RESULT := "Hello World!"
 
 lua_dep_LUA_DEPS := plop:lua_dep/foo.lua \
 	lua_dep/main.lua
+lua_dep_CHECK_RESULT := "Hello World!"
 
 no_dep_LUA_DEPS := no_dep/main.lua
+no_dep_CHECK_RESULT := "Hello luawrapper world !"
